cd31288125a1fdebf212c63189bffcfc86ef5f53
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2025-08-16 14:55:14 +0200

    Add Pinia for state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   pages: true,
   devtools: { enabled: true },
-  modules: ['@nuxtjs/ionic'],
+  modules: ['@nuxtjs/ionic', '@pinia/nuxt'],
   ssr: false,
   css: ['~/assets/css/main.css'],
   vite: {
